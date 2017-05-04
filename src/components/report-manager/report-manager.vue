3fc07fdb17e2da4a23512d22ca080b6c1661d435
shannonliang <shannonliang312@gmail.com>
2017-05-04 18:27:33 +0800

    update

--- src/components/report-manager/report-manager.vue
+++ src/components/report-manager/report-manager.vue
@@ -4,14 +4,22 @@
       报表管理
     </div>
     <div class="router-box-body">
-
+      <el-button type="primary" @click="exportExcel">
+        导出Excel
+      </el-button>
     </div>
   </div>
 </template>
 
 <script>
+  import { export_json_to_excel } from '@/service/excel/Export2Excel'
   export default {
-    name: "report-manager"
+    name: "report-manager",
+    methods: {
+      exportExcel() {
+        export_json_to_excel();
+      }
+    }
   }
 </script>
 
